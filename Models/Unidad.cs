8b8b11755e05f2c8997a918abf04db776db6047e
Joseph Daniel Hernandez Estrada <jestrada1997@hotmail.com>
2020-11-25 23:52:58 -0600

    se agrega info para desplegar en unidad

--- Models/Unidad.cs
+++ Models/Unidad.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace MovilidadInteligenteUI.Models
 {
     public class Unidad
     {
+        [Display(Name = "ID")]
         public string idUnidad { get; set; }
+
+        [Display(Name = "Descripcion")]
         public string descripcion { get; set; }
+
+        [Display(Name = "Placa")]
         public string placa { get; set; }
+        
+        [Display(Name = "Estado")]
         public bool estado { get; set; }
     }
 }
